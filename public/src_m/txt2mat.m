9fff5e286ca3c3031d7fbabbab1f8801330af9ff
goettsche <goettsche@486fb3b8-ece9-11e5-9c3d-8df3af046faa>
2016-11-02 13:10:23 +0000

    svn property for Keywords activated
    git-svn-id: https://svn.noc.fh-aachen.de/carnot/trunk@81 486fb3b8-ece9-11e5-9c3d-8df3af046faa

--- public/src_m/txt2mat.m
+++ public/src_m/txt2mat.m
@@ -542,7 +542,7 @@ function [A,ffn,numHeader,repChar,hl,fpos] = txt2mat(varargin)
 
 % --- Author: -------------------------------------------------------------
 %   Copyright 2005-2012 Andres
-%   $Revision:   1.3  $  $Date:   Mar 15 2013 09:55:56  $
+%   $Revision$  $Date$
 % Redistribution and use in source and binary forms, with or without
 % modification, are permitted provided that the following conditions are
 % met:
@@ -1705,7 +1705,7 @@ function [anaNumHeader, anaNumColon, anaFormat, anaRepChar, anaReadMode, ...
 % arg           txt2mat's input argument struct
 
 %   Copyright 2006-2012 Andres
-%   $Revision:   1.3  $  $Date:   Mar 15 2013 09:55:56  $
+%   $Revision$  $Date$
 
 % some preparations
 ffn             = arg.val.FileName;
@@ -2227,7 +2227,7 @@ function [w, newidcoi, vi] = cutvec(v,li,hi,doKeep,varargin)
 % 
 % [w, newidcoi, vi] = cutvec(v,li,hi,doKeep,idcoi)
 
-%   $Revision:   1.3  $ 
+%   $Revision$ 
 
 lenV   = numel(v);
 has_idcoi = false;
@@ -2607,7 +2607,7 @@ function [idcLb, cntLb, secLbIdc, idcBad, idcGood] = FindLineBreaks(f8, uintLb, 
 %               badStrings(1:numBad) 
 % goodStrings   cell array containing the good line marker strings.
 
-%   $Revision:   1.3  $ 
+%   $Revision$ 
 
 lenF8   = numel(f8);
 idxLo 	= 1;   % init., start index of a section processed in a loop
